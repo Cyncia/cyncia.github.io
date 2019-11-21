be99793f4d0f7198b6a1ce50aed8835a9ff6274f
Cyncia <cynciazhou@gmail.com>
2019-11-20 22:37:48 -0800

    setting up repo

---
+++ script/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $("#nav-placeholder").load("../html/navigation.html");
+ });
+
+$(document).ready(function () {
+    $("#footer-placeholder").load("../html/footer.html");
+ });
