06a066e086db714649a3e416b17e6d4ecf23bef9
Cyncia <cynciazhou@gmail.com>
2019-11-24 22:47:45 -0800

    fixed active navbar link and made pdf window responsive

--- script/script.js
+++ script/script.js
@@ -1,7 +1,11 @@
+// Load Navigation Bar
 $(document).ready(function () {
     $("#nav-placeholder").load("../html/navigation.html");
  });
 
+
+// Load Footer Section
 $(document).ready(function () {
     $("#footer-placeholder").load("../html/footer.html");
  });
+
