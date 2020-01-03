68aaa8b327bbfd32f86ba366bd2027fb9adf4050
Cyncia <cynciazhou@gmail.com>
2020-01-03 18:27:24 -0500

    update to latest version

--- script/script.js
+++ script/script.js
@@ -9,3 +9,17 @@ $(document).ready(function () {
     $("#footer-placeholder").load("../html/footer.html");
  });
 
+$(document).ready( function () {
+ $('#down').on('click',function () {
+  $("html").scrollTop(0);
+   $('html, body').animate({
+     scrollTop: $('#projects').offset().top
+ }, 10);
+ });
+});
+
+/*
+window.addEventListener("beforeunload", function() {
+    document.body.classList.add("animate-out");
+});
+*/
